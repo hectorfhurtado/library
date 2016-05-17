caaa6a4acdf3c5b1af410f8d7531f7693966b8ef
hectorfhurtado <hurtado.fernando@gmail.com>
2016-05-16 21:18:38 -0500

    Si no existe la carpeta ebooks, la crea

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const http      = require( 'http' )
 const Servidor  = require( './servidor' )
+const Libro     = require( '../lib/libro')
 const server    = http.createServer(( req, res ) => Servidor.sirve( req, res ))
 
 server.listen( 7010 )
+
+Libro.verificaSiExisteEbooks()
 
 console.log( 'http://localhost:7010' )
